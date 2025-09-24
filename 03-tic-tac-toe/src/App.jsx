92bf101531d09f50ef99f3aed683854795b7abe7
anksatya <ankit.satyam@publicissapient.com>
2025-09-24 10:32:40 +0530

    reduce state management and derive state if possible

--- 03-tic-tac-toe/src/App.jsx
+++ 03-tic-tac-toe/src/App.jsx
@@ -4,23 +4,33 @@ import GameBoard from "./components/GameBoard";
 import Player from "./components/Player";
 import Log from "./components/Log";
 
+function derivedActivePlayer(gameTurns) {
+  let currPlayer = 'X';
+
+  if (gameTurns.length > 0 && gameTurns[0].player === 'X') {
+    currPlayer = 'O';
+  }
+
+  return currPlayer;
+}
+
 function App() {
-  const [activePlayer, setActivePlayer] = useState("X");
+  // This state is removed as this can be derived.
+  // const [activePlayer, setActivePlayer] = useState("X");
   // This is going to have similar data to what we have in gameBoard. Avoid intersecting states.
   const [gameTurns, setGameTurns] = useState([]);
 
+  const activePlayer = derivedActivePlayer(gameTurns);
+
   function handleSelectSquare(rowIndex, colIndex) {
-    setActivePlayer((currActivePlayer) =>
-      currActivePlayer === "X" ? "O" : "X"
-    );
+    // setActivePlayer((currActivePlayer) =>
+    //   currActivePlayer === "X" ? "O" : "X"
+    // );
     setGameTurns((prevTurns) => {
-      let currPlayer = 'X';
-      if (prevTurns.length > 0 && prevTurns[0].player === 'X') {
-        currPlayer = 'O';
-      }
+      const currPlayer = derivedActivePlayer(prevTurns);
 
       const updatedTurns = [
-        { square: { row: rowIndex, col: colIndex }, player: activePlayer },
+        { square: { row: rowIndex, col: colIndex }, player: currPlayer },
         ...prevTurns,
       ];
       
