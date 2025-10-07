cc29dc61a14c498fe8ebe69d9aa7a124b25c3ac3
anksatya <ankit.satyam@publicissapient.com>
2025-10-07 15:44:11 +0530

    add user input component

--- 04-react-essentials-practice-project/src/App.jsx
+++ 04-react-essentials-practice-project/src/App.jsx
@@ -1,9 +1,30 @@
-import Header from "./components/Header"
+import { useState } from "react";
+import Header from "./components/Header";
+import UserInput from "./components/UserInput";
 
 function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 0,
+    annualInvestment: 0,
+    expectedReturn: 0,
+    duration: 0,
+  });
+
+  function handleChange(input, newValue) {
+    setUserInput((prevInput) => {
+      return {
+        ...prevInput,
+        [input]: newValue,
+      };
+    });
+  }
+
   return (
-    <Header />
-  )
+    <>
+      <Header />
+      <UserInput input={userInput} onChange={handleChange} />
+    </>
+  );
 }
 
-export default App
+export default App;
